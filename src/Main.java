a3d25ec098d7c581440f0a9df7c82594afb2789d
SoseD-11 <kost1991ya@mail.ru>
2022-09-22 15:30:52 +0300

    Циклы-ДЗ1 ДЗ2 ДЗ-3 готово

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание -1");
         System.out.println("Задание 1");
-        for (int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         System.out.println("Задание 2");
-        for (int i = 10; i >= 1; i--){
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
         System.out.println("Задание 3");
@@ -19,11 +19,11 @@ public class Main {
         }
         System.out.println("Домашнее задание -2");
         System.out.println("Задание 1");
-        for (int i = 1904; i <= 2096; i = i + 4){
+        for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i);
         }
         System.out.println("Задание 2");
-        for (int i = 7; i <= 98; i = i + 7){
+        for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(i);
         }
         System.out.println("Задание 3");
@@ -31,6 +31,24 @@ public class Main {
             System.out.println(i);
         }
         System.out.println("Домашнее задание -3");
+        System.out.println("Задание 1");
+        int accumulation = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + accumulation;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Задание 2");
+        int contribution = 29000;
+        int allMoney = 0;
+        for (int i = 1; i <= 12; i++) {
+            allMoney = allMoney + allMoney/100;
+            allMoney = allMoney + contribution;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + allMoney + " рублей");
+        }
+
+
+
 
     }
 }
