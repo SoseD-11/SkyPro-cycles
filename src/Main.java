cd4643aa31ba0fcb757f4190844aea8ede85791e
SoseD-11 <kost1991ya@mail.ru>
2022-09-22 14:33:37 +0300

    Циклы-ДЗ1 и ДЗ2 готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Домашнее задание -1");
+        System.out.println("Задание 1");
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        System.out.println("Задание 2");
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+        System.out.println("Задание 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        System.out.println("Задание 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Домашнее задание -2");
+        System.out.println("Задание 1");
+        for (int i = 1904; i <= 2096; i = i + 4){
+            System.out.println(i);
+        }
+        System.out.println("Задание 2");
+        for (int i = 7; i <= 98; i = i + 7){
+            System.out.println(i);
+        }
+        System.out.println("Задание 3");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+        System.out.println("Домашнее задание -3");
+
     }
 }
